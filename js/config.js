036998c1994e3a7878f549b9315de628f34da09f
will-coleman <33788372+will-coleman@users.noreply.github.com>
2026-08-27 13:50:12 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -368,7 +368,7 @@ window.SITE_CONFIG = {
       },
       {
         name: 'Roman Empire',
-        flag: 'assets/flags/Rom.png‎',
+        flag: 'assets/Rom.png‎',
         links: [
           { label: 'Instagram', url: 'https://www.instagram.com/romanempireirl/' }
         ]
